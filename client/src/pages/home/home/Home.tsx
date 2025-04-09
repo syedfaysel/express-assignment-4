05c625545a9d55ad8c366c55a811de53c64235e2
nasif07 <nasifulislamnasif23@gmail.com>
2025-04-09 22:06:45 +0600

    created footer

--- client/src/pages/home/home/Home.tsx
+++ client/src/pages/home/home/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from '../banner/Banner';
 import FeaturedProducts from '../featuredProducts/FeaturedProducts';
+import Testimonials from '../testimonials/Testimonials';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <FeaturedProducts></FeaturedProducts>
+            <Testimonials></Testimonials>
         </div>
     );
 };
